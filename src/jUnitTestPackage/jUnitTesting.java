d04415f65ffdc4d6456e4f3aab5fbed0aaf7b819
AkshatJain20092002 <E21CSEU0164@bennett.edu.in>
2022-11-18 17:49:35 +0530

    Commit.

---
+++ src/jUnitTestPackage/jUnitTesting.java
@@ -0,0 +1,12 @@
+package jUnitTestPackage;
+
+public class jUnitTesting {
+	public int AddNumbers(int a,int b) {
+		int ans=a+b;
+		return ans;
+	}
+	public String AddStrings(String a,String b) {
+		String ans=a+b;
+		return ans;
+	}
+}
